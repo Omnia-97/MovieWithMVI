8263108e967ffb5c1c18b1020ccc789b8ed16dc5
DELL <omnianabil1920@gmail.com>
2024-09-02 17:39:45 +0300

    implement view model and view

--- app/src/main/java/com/example/mvipractice/ui_layer/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/mvipractice/ui_layer/viewmodel/MainViewModel.kt
@@ -1,4 +1,35 @@
 package com.example.mvipractice.ui_layer.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvipractice.data_layer.repo.RepoImpl
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class MainViewModel @Inject constructor(
+    private val repoImpl: RepoImpl
+) : ViewModel() {
+    private val _state = MutableStateFlow(MovieViewState())
+    val state: StateFlow<MovieViewState> = _state
+
+    fun handleIntent(intent: MovieIntent) {
+        viewModelScope.launch {
+            when (intent) {
+                is MovieIntent.LoadsMovies -> fetchMovies()
+            }
+        }
+    }
+
+    private suspend fun fetchMovies() {
+        _state.value = _state.value.copy(loading = true, error = null)
+        try {
+            val movies = repoImpl.getMovies()
+            _state.value = MovieViewState(loading = false, movies = movies)
+        } catch (e: Exception) {
+            _state.value =
+                MovieViewState(loading = false, error = e.message ?: "Error fetching movies")
+        }
+    }
 }
--- app/src/main/java/com/example/mvipractice/ui_layer/views/MovieScreen.kt
+++ app/src/main/java/com/example/mvipractice/ui_layer/views/MovieScreen.kt
@@ -1,2 +1,74 @@
 package com.example.mvipractice.ui_layer.views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.mvipractice.data_layer.domain.Movie
+import com.example.mvipractice.ui_layer.viewmodel.MainViewModel
+import com.example.mvipractice.ui_layer.viewmodel.MovieIntent
+
+@Composable
+fun MovieScreen(mainViewModel: MainViewModel) {
+    val state by mainViewModel.state.collectAsState()
+
+    LaunchedEffect(mainViewModel) {
+        mainViewModel.handleIntent(MovieIntent.LoadsMovies)
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        when {
+            state.loading -> {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+            }
+
+            state.error != null -> {
+                Text(text = "Error: ${state.error}", color = Color.Red)
+            }
+
+            else -> {
+                MovieList(movies = state.movies)
+            }
+        }
+
+    }
+}
+
+@Composable
+fun MovieList(movies: List<Movie>) {
+    LazyColumn {
+        items(movies) { movie ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .shadow(4.dp, RoundedCornerShape(8.dp))
+            ) {
+                Text(text = "Movie : ${movie.title}", modifier = Modifier.padding(4.dp))
+                Text(text = "Date : ${movie.year}", modifier = Modifier.padding(4.dp))
+
+            }
+
+        }
+
+    }
+
+}
